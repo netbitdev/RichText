264724fdca27c5dd4ae56e304aada6ef4f2a488d
netbitdev <85470175+netbitdev@users.noreply.github.com>
2022-06-14 12:08:20 -0500

    Update jquery.richtext.js
    Removed insertAtCursor() (didn't work).

--- src/jquery.richtext.js
+++ src/jquery.richtext.js
@@ -1867,27 +1867,6 @@
             }
         }
 
-function insertAtCursor(myField, myValue) {
-    //IE support
-    if (document.selection) {
-        myField.focus();
-        sel = document.selection.createRange();
-        sel.text = myValue;
-    }
-    //MOZILLA and others
-    else if (myField.selectionStart || myField.selectionStart == '0') {
-        var startPos = myField.selectionStart;
-        var endPos = myField.selectionEnd;
-        myField.value = myField.value.substring(0, startPos)
-            + myValue
-            + myField.value.substring(endPos, myField.value.length);
-        myField.selectionStart = startPos + myValue.length;
-        myField.selectionEnd = startPos + myValue.length;
-    } else {
-        myField.value += myValue;
-    }
-}
-
         /**
          * Paste HTML at caret position
          * @param {string} html HTML code
